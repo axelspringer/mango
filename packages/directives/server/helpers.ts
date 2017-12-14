2ff5aae617e7928afafd6c0c3117aeb58dc049fd
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-12-14 13:32:47 +0100

    ~ 0.0.1-alpha

--- packages/directives/server/helpers.ts
+++
@@ -1,9 +0,0 @@
-import * as Path from 'path'
-import * as express from 'express'
-
-export const isProd = process.env.NODE_ENV === 'production'
-export const resolve = file => Path.resolve(__dirname, file)
-export const serve = (path, cache) => express.static(resolve(path), {
-  // evalute times on prod
-  maxAge: cache && isProd ? 60 * 60 * 24 * 30 : 0
-})
