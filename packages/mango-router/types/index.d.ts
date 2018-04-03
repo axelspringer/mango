b9f482eea4eab665f779c6d0870b3c45d1af8286
Döll, Sebastian <sebastian.doell@spring-media.de>
2018-04-03 16:10:23 +0200

    v0.6.5

---
+++ packages/mango-router/types/index.d.ts
@@ -0,0 +1,59 @@
+import { Component } from 'vue'
+import { RouteConfig } from 'vue-router/types'
+
+declare namespace MangoRouter {
+  type ResolverFunc = (cmp: Component) => Component
+  type Name = String
+  type Option = 'name'
+    | 'components'
+    | 'redirect'
+    | 'props'
+    | 'alias'
+    | 'children'
+    | 'beforeEnter'
+    | 'meta'
+    | 'caseSensitive'
+    | 'pathToRegexpOptions'
+    | 'as'
+    | 'guard'
+    | 'prefix'
+
+  interface Options {
+  }
+}
+
+declare class Route {
+  public cmp: Component
+  public config: RouteConfig
+
+  constructor(public cmp: Component, public config: RouteConfig) { }
+
+  public static path: String
+  public static default: any
+}
+
+declare class Router {
+  constructor(public options: MangoRouter.Options = {})
+
+  static install: (Vue) => void
+  static version: string
+
+  public resolver: MangoRouter.ResolverFunc
+
+  public init(app: any)
+
+  public language(cmp: Component): Router
+  public home(cmp: Component): Router
+  public category(cmp: Component): Router
+  public day(cmp: Component): Router
+  public month(cmp: Component): Router
+  public post(cmp: Component): Router
+  public tag(cmp: Component): Router
+  public year(cmp: Component): Router
+
+  public all(): RouteConfig[]
+}
+
+export default Router
+
+
