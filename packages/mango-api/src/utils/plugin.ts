d849916c6a2f5a6b067e5a9303c1f9f56fc7161f
Mario Bartlack <mario.bartlack@axelspringer.de>
2018-04-10 14:13:09 +0200

    Refactoring mango-plugin module loader

--- packages/mango-api/src/utils/plugin.ts
+++ packages/mango-api/src/utils/plugin.ts
@@ -1,4 +1,21 @@
+const Repository = require('lerna/lib/Repository')
+const PackageUtilities = require('lerna/lib/PackageUtilities')
+
 export const loadPlugin = plugin => {
-  const { main } = require(`../../../mango-plugin-${plugin}/package.json`)
-  return require(`../../../mango-plugin-${plugin}/${main}`)
+  const repo = new Repository()
+  const loadedPackages = PackageUtilities.getPackages({
+    rootPath: repo.rootPath,
+    packageConfigs: [
+      `${repo.rootPath}/node_modules/@axelspringer/mango-plugin-${plugin}`,
+      `packages/mango-plugin-${plugin}`
+    ]
+  })
+
+  if (!loadedPackages.length) {
+    throw new Error(`Requested plugin ${plugin} not found`)
+  }
+
+  const pack = loadedPackages[0];
+
+  return require(`${pack.location}/${pack.toJSON().main}`)
 }
