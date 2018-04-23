7d8ea690a2ee509a9cffef4e6606813d36120c44
Steve Junker <steve.junker@axelspringer.de>
2018-04-23 15:43:09 +0200

    ~ added media and category as root query

--- packages/mango-api/src/type/queryType.ts
+++ packages/mango-api/src/type/queryType.ts
@@ -1,6 +1,9 @@
 const { GraphQLString, GraphQLList } = require('graphql')
+
 import { PostType } from './postType'
 import { SettingsType } from './settingsType'
+import { CategoryType } from './catType'
+import { MediaType } from './mediaType'
 
 export const defaultQuery = {
   posts: {
@@ -15,6 +18,24 @@ export const defaultQuery = {
   settings: {
     type: SettingsType,
     resolve: (_root, _args, ctx) => ctx.loader.getSettings(ctx)
+  },
+  category: {
+    type: CategoryType,
+    args: {
+      id: {
+        type: GraphQLString
+      }
+    },
+    resolve: (_root, _args, ctx) => ctx.loader.getCategory(ctx, _args.id, _args)
+  },
+  media: {
+    type: MediaType,
+    args: {
+      id: {
+        type: GraphQLString
+      }
+    },
+    resolve: (_root, _args, ctx) => ctx.loader.getMedia(ctx, _args.id, _args)
   }
 }
 
