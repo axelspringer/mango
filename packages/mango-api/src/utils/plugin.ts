091f1dd4072393b82d4c289413692682e9a99c60
Mario Bartlack <mario.bartlack@axelspringer.de>
2018-04-24 10:29:31 +0200

    Rely on globally installed mango-plugins in production env

--- packages/mango-api/src/utils/plugin.ts
+++ packages/mango-api/src/utils/plugin.ts
@@ -2,14 +2,22 @@ const Repository = require('lerna/lib/Repository')
 const PackageUtilities = require('lerna/lib/PackageUtilities')
 
 export const loadPlugin = plugin => {
+  if (process.env.NODE_ENV !== 'development') {
+    try {
+      // try require globally installed module
+      const { main } = require(`@axelspringer/mango-plugin-${plugin}/package.json`)
+      return require(`@axelspringer/mango-plugin-${plugin}/${main}`)
+    } catch (e) {
+      throw new Error(`Requested plugin ${plugin} not found. Is it installed globally?`)
+    }
+  }
+
   const repo = new Repository()
   const loadedPackages = PackageUtilities.getPackages({
     rootPath: repo.rootPath,
     packageConfigs: [
       `${repo.rootPath}/node_modules/@axelspringer/mango-plugin-${plugin}`,
-      `packages/mango-plugin-${plugin}`,
-      `/usr/local/lib/node_modules/@axelspringer/mango-plugin-${plugin}`,
-      `node_modules/@axelspringer/mango-plugin-${plugin}`,
+      `packages/mango-plugin-${plugin}`
     ]
   })
 
