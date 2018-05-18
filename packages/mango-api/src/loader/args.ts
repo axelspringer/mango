9f5744834fd2e701d5c4eae51cdb3484c857d508
Döll, Sebastian <sebastian.doell@spring-media.de>
2018-05-18 14:13:06 +0200

    ~ adding type

---
+++ packages/mango-api/src/loader/args.ts
@@ -0,0 +1,5 @@
+export type PostArguments = {
+  permalink?: string
+  _embed?: boolean
+  id?: number
+}
--- packages/mango-api/src/type/queryType.ts
+++ packages/mango-api/src/type/queryType.ts
@@ -13,10 +13,33 @@ export default {
     args: {
       lang: {
         type: GraphQLString
+      },
+      permalink: {
+        type: GraphQLString
+      },
+      _embed: {
+        type: GraphQLBoolean
       }
     },
     resolve: (_, args, ctx) => ctx.loader.getPosts(ctx, args)
   },
+
+  post: {
+    type: PostType,
+    args: {
+      lang: {
+        type: GraphQLString
+      },
+      permalink: {
+        type: GraphQLString
+      },
+      _embed: {
+        type: GraphQLBoolean
+      }
+    },
+    resolve: (_, args, ctx) => ctx.loader.getPost(ctx, args)
+  },
+
   postListById: {
     type: new GraphQLList(PostType),
     args: {
@@ -26,6 +49,7 @@ export default {
     },
     resolve: (_, args, ctx) => ctx.loader.getPostListById(ctx, args.id, args)
   },
+
   postListByCategoryId: {
     type: new GraphQLList(PostType),
     args: {
@@ -58,14 +82,17 @@ export default {
     },
     resolve: (_root, args, ctx) => ctx.loader.getCategoryByPermalink(ctx, args.permalink, args)
   },
+
   settings: {
     type: SettingsType,
     resolve: (_root, _args, ctx) => ctx.loader.getSettings(ctx)
   },
+
   terms: {
     type: TermType,
     resolve: (_root, _args, ctx) => ctx.loader.getTerms(ctx)
   },
+
   category: {
     type: CategoryType,
     args: {
@@ -75,6 +102,7 @@ export default {
     },
     resolve: (_root, _args, ctx) => ctx.loader.getCategory(ctx, _args.id, _args)
   },
+
   media: {
     type: MediaType,
     args: {
