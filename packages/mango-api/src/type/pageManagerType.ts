09e0333542dbfc8e5878efa888ba22ca22a0766b
Döll, Sebastian <sebastian.doell@axelspringer.com>
2018-05-24 18:53:13 +0200

    ~ fixing types

--- packages/mango-api/src/type/pageManagerType.ts
+++ packages/mango-api/src/type/pageManagerType.ts
@@ -1,7 +1,7 @@
 const { GraphQLScalarType } = require('graphql')
 
 export default new GraphQLScalarType({
-  name: 'PageManager',
+  name: 'PageManagerFields',
   description: 'Contains the Page Manager Settings',
   parseValue: value => value, // not yet send anything from the client
   serialize: pm => !Array.isArray(pm) ? pm : {},
