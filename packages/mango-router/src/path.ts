434b0fe5c5e203c5936a988d48c98ffd48dc3696
Döll, Sebastian <sebastian.doell@axelspringer.com>
2018-05-28 18:20:25 +0200

    ~ remove optional lang

--- packages/mango-router/src/path.ts
+++ packages/mango-router/src/path.ts
@@ -1,6 +1,6 @@
 export default class Path {
   public static Home = ''
-  public static Language = '/:lang?'
+  public static Language = '/:lang'
   public static Category = '/:wpCategory'
   public static Day = '/:wpDay'
   public static Month = '/:wpMonth'
