8b671314b5f33d0d4bac0304619adfc9be8a9680
Döll, Sebastian <sebastian.doell@axelspringer.com>
2018-06-05 06:27:18 +0200

    ~ check translations

--- packages/mango-api/src/type/polylangTranslationType.ts
+++ packages/mango-api/src/type/polylangTranslationType.ts
@@ -4,6 +4,7 @@ export default new GraphQLScalarType({
   name: 'PolylangTranslation',
   description: 'Contains related Polylang translations',
   parseValue: value => value, // not yet send anything from the client
-  serialize: translations => translations.reduce((trans, data) => ({ ...trans, [data.lang]: data }), {}),
+  serialize: translations => translations !== null ?
+    translations.reduce((trans, data) => ({ ...trans, [data.lang]: data }), {}) : translations,
   parseLiteral: ast => ast.value
 })
