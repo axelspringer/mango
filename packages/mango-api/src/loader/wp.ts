299806ddaad4c398d63b06b5c51b29bceed96cd2
Döll, Sebastian <sebastian.doell@axelspringer.com>
2018-06-07 14:54:11 +0200

    ~ fixing minor issue on tags

--- packages/mango-api/src/loader/wp.ts
+++ packages/mango-api/src/loader/wp.ts
@@ -69,12 +69,13 @@ export default class WP extends Loader {
   // fetch tag
   public async getTag(ctx: GraphQLContext, id: number, args: ListTags = {}, type: Type = 'Array') {
     const res = await this._fetcher(ctx, !id ? API.Tags : [API.Tags, id].join('/'), args)
+    console.log(res)
     return type === 'Array' ? Array.isArray(res) ? res : res !== null ? [res] : res : Array.isArray(res) && res.length === 1 ? res[0] : res
   }
 
   // fetch tags
   public async getTags(ctx: GraphQLContext, ids: [number], args: ListTags = {}) {
-    return Promise.all([...ids.map(id => this.getTag(ctx, id, args))])
+    return Promise.all([...ids.map(id => this.getTag(ctx, id, args, 'Object'))])
   }
 
   // fetch media
