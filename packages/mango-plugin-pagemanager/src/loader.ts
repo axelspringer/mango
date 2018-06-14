b7051409c73de0f5ec886b9789925af0495623a1
Toni Medina <toni.medina@spring-media.de>
2018-06-14 18:56:03 +0200

    Queries optimized

--- packages/mango-plugin-pagemanager/src/loader.ts
+++ packages/mango-plugin-pagemanager/src/loader.ts
@@ -2,38 +2,39 @@ import { GraphQLContext } from 'graphql'
 import API from './api'
 
 export default {
-  // fetch page manager categories
-  getPageManagerCategories: async function (ctx: GraphQLContext, id: number, args = {}) {
-    return await this._fetcher(ctx, [API.PageManagerCategories, id].join('/'), args)
-  },
+  async pageManagerBlockByPageTypeResolver(ctx: GraphQLContext, args = { id: 0, language: '', section: '', currentPageType: '' }) {
+    if (0 === Object.keys(args).length && Object === args.constructor) {
+      return null
+    }
 
-  // fetch page manager tags
-  getPageManagerTags: async function (ctx: GraphQLContext, id: number, args = {}) {
-    return await this._fetcher(ctx, [API.PageManagerTags, id].join('/'), args)
-  },
+    if ('category' === args.currentPageType && args.id) {
+      return await this._fetcher(ctx, [API.PageManagerCategories, args.id].join('/'), args)
+    }
 
-  // fetch page manager posts
-  getPageManagerPosts: async function (ctx: GraphQLContext, id: number, args = {}) {
-    return await this._fetcher(ctx, [API.PageManagerPosts, id].join('/'), args)
-  },
+    if ('tag' === args.currentPageType && args.id) {
+      return await this._fetcher(ctx, [API.PageManagerTags, args.id].join('/'), args)
+    }
 
-  // fetch page manager pages
-  getPageManagerPages: async function (ctx: GraphQLContext, id: number, args = {}) {
-    return await this._fetcher(ctx, [API.PageManagerPages, id].join('/'), args)
-  },
+    if ('post' === args.currentPageType && args.id) {
+      return await this._fetcher(ctx, [API.PageManagerPosts, args.id].join('/'), args)
+    }
 
-  // fetch page manager home
-  getPageManagerHome: async function (ctx: GraphQLContext, language: string, args = {}) {
-    return await this._fetcher(ctx, [API.PageManagerGlobal, 'home', language].join('/'), args)
-  },
+    if ('page' === args.currentPageType && args.id) {
+      return await this._fetcher(ctx, [API.PageManagerPages, args.id].join('/'), args)
+    }
 
-  // fetch page manager data for global settings (e.g. for home)
-  getPageManagerGlobal: async function (ctx: GraphQLContext, section: string, language: string, args = {}) {
-    return await this._fetcher(ctx, [API.PageManagerGlobal, section, language].join('/'), args)
-  },
+    if ('home' === args.currentPageType && args.language) {
+      return await this._fetcher(ctx, [API.PageManagerGlobal, 'home', args.language].join('/'), args)
+    }
 
-  // fetch available languages
-  getPageManagerLanguages: async function (ctx: GraphQLContext, args = {}) {
-    return await this._fetcher(ctx, API.PageManagerLanguages, args)
-  }
+    if ('global' === args.currentPageType && args.section && args.language) {
+      return await this._fetcher(ctx, [API.PageManagerGlobal, args.section, args.language].join('/'), args)
+    }
+
+    if ('language' === args.currentPageType) {
+      return await this._fetcher(ctx, API.PageManagerLanguages, args)
+    }
+
+    return null
+  },
 }
--- packages/mango-plugin-pagemanager/src/query.ts
+++ packages/mango-plugin-pagemanager/src/query.ts
@@ -2,150 +2,15 @@ import { PageManager, PageManagerLanguage } from './types'
 const { GraphQLString, GraphQLInt, GraphQLList } = require('graphql')
 
 export default {
-  pageManagerHome: {
+  pageManagerBlockByPageType: {
     type: PageManager,
     description: 'Returns the PageManager block of the home',
     args: {
-      language: {
-        description: 'The language to return the PageManager blocks for',
-        type: GraphQLString
-      },
-      block_name: {
-        description: 'The block name, which result should be returned',
-        type: GraphQLString
-      },
-      block_id: {
-        description: 'The block id (order id), which result should be returned',
-        type: GraphQLInt
-      },
-      page: {
-        description: 'The current page number for pagination',
-        type: GraphQLInt
-      },
-      year: {
-        description: 'The year to return the posts for',
-        type: GraphQLInt
-      }
-    },
-    resolve: (_root, args, ctx) => ctx.loader.getPageManagerHome(ctx, args.language, args)
-  },
-  pageManagerCategory: {
-    type: PageManager,
-    args: {
       id: {
         type: GraphQLInt
       },
       language: {
         description: 'The language to return the PageManager blocks for',
-        type: GraphQLString
-      },
-      block_name: {
-        description: 'The block name, which result should be returned',
-        type: GraphQLString
-      },
-      block_id: {
-        description: 'The block id (order id), which result should be returned',
-        type: GraphQLInt
-      },
-      page: {
-        description: 'The current page number for pagination',
-        type: GraphQLInt
-      },
-      year: {
-        description: 'The year to return the posts for',
-        type: GraphQLInt
-      }
-    },
-    resolve: (_root, args, ctx) => ctx.loader.getPageManagerCategories(ctx, args.id, args)
-  },
-  pageManagerTag: {
-    type: PageManager,
-    args: {
-      id: {
-        type: GraphQLInt
-      },
-      language: {
-        type: GraphQLString
-      },
-      block_name: {
-        description: 'The block name, which result should be returned',
-        type: GraphQLString
-      },
-      block_id: {
-        description: 'The block id (order id), which result should be returned',
-        type: GraphQLInt
-      },
-      page: {
-        description: 'The current page number for pagination',
-        type: GraphQLInt
-      },
-      year: {
-        description: 'The year to return the posts for',
-        type: GraphQLInt
-      }
-    },
-    resolve: (_root, args, ctx) => ctx.loader.getPageManagerTags(ctx, args.id, args)
-  },
-  pageManagerPage: {
-    type: PageManager,
-    args: {
-      id: {
-        type: GraphQLInt
-      },
-      language: {
-        type: GraphQLString
-      },
-      block_name: {
-        description: 'The block name, which result should be returned',
-        type: GraphQLString
-      },
-      block_id: {
-        description: 'The block id (order id), which result should be returned',
-        type: GraphQLInt
-      },
-      page: {
-        description: 'The current page number for pagination',
-        type: GraphQLInt
-      },
-      year: {
-        description: 'The year to return the posts for',
-        type: GraphQLInt
-      }
-    },
-    resolve: (_root, args, ctx) => ctx.loader.getPageManagerPages(ctx, args.id, args)
-  },
-  pageManagerPost: {
-    type: PageManager,
-    args: {
-      id: {
-        type: GraphQLInt
-      },
-      language: {
-        type: GraphQLString
-      },
-      block_name: {
-        description: 'The block name, which result should be returned',
-        type: GraphQLString
-      },
-      block_id: {
-        description: 'The block id (order id), which result should be returned',
-        type: GraphQLInt
-      },
-      page: {
-        description: 'The current page number for pagination',
-        type: GraphQLInt
-      },
-      year: {
-        description: 'The year to return the posts for',
-        type: GraphQLInt
-      }
-    },
-    resolve: (_root, args, ctx) => ctx.loader.getPageManagerPosts(ctx, args.id, args)
-  },
-  pageManagerGlobal: {
-    type: PageManager,
-    args: {
-      language: {
         type: GraphQLString
       },
       section: {
@@ -166,12 +31,12 @@ export default {
       year: {
         description: 'The year to return the posts for',
         type: GraphQLInt
+      },
+      currentPageType: {
+        description: 'The current page type',
+        type: GraphQLString
       }
     },
-    resolve: (_root, args, ctx) => ctx.loader.getPageManagerGlobal(ctx, args.section, args.language, args)
-  },
-  pageManagerLanguages: {
-    type: new GraphQLList(PageManagerLanguage),
-    resolve: (_root, args, ctx) => ctx.loader.getPageManagerLanguages(ctx, args)
+    resolve: (_root, args, ctx) => ctx.loader.pageManagerBlockByPageTypeResolver(ctx, args)
   }
 }
