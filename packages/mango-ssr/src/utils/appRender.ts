140136e80db4a79d862680a5976358beac9fbfed
Döll, Sebastian <sebastian.doell@axelspringer.com>
2018-06-14 06:40:37 +0200

    ~ testing micro caching strategy

--- packages/mango-ssr/src/utils/appRender.ts
+++ packages/mango-ssr/src/utils/appRender.ts
@@ -1,5 +1,13 @@
 import SSRContext from '../context'
 import setHeaders from './setHeaders'
+import * as LRU from 'lru-cache'
+
+const microCache = LRU({
+  max: 100,
+  maxAge: 1 * 1000 // 1s
+})
+
+const isCacheable = req => req.method === 'GET'
 
 function render(renderer, ctx, context) {
   return new Promise((resolve, reject) => {
@@ -15,7 +23,7 @@ function render(renderer, ctx, context) {
       resolve(html)
     }) // wait to render string
   })
-    .then(html => ctx.body = html) //
+    .then(html => html) //
     .catch(err => ctx.throw(500, err))
 }
 
@@ -30,7 +38,23 @@ export default async (ctx, next) => {
   }
 
   await next() // wait for downstream
-  await render(renderer, ctx, context) // wait for render
+
+  const cacheable = isCacheable(ctx.req)
+  if (cacheable) {
+    const hit = microCache.get(ctx.req.url)
+    if (hit) {
+      ctx.body = hit
+    }
+
+    if (!hit) {
+      ctx.body = await render(renderer, ctx, context) // wait for render
+      microCache.set(ctx.req.url, ctx.body)
+    }
+  }
+
+  if (!cacheable) {
+    await render(renderer, ctx, context) // wait for render
+  }
 
   setHeaders(ctx, { 'Content-Type': 'text/html' })
 }
