8c4d39fecbe9501ffdd5d95985c22074267bb3e3
Döll, Sebastian <sebastian@katallaxie.me>
2018-06-27 05:04:31 +0200

    ~ support async plugin render function

--- packages/mango-ssr/src/utils/asyncRender.ts
+++ packages/mango-ssr/src/utils/asyncRender.ts
@@ -1,8 +1,8 @@
 import { Renderer } from 'vue-server-renderer/types'
 
 export default function (renderer: Renderer, render: Function, ctx: Object) {
-  return new Promise((resolve) => {
-    const cmp = typeof render === 'function' ? render(ctx) : render
+  return new Promise(async (resolve) => {
+    const cmp = typeof render === 'function' ? await render(ctx) : render
     resolve(renderer.renderToString(cmp, ctx) as any)
   })
 }
