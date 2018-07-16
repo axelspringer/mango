ba25a4e485e275b3d8aeea0ac6bd427a4095702e
Döll, Sebastian <sebastian@katallaxie.me>
2018-07-16 14:59:27 +0200

    feat(pm) ~ find in results, render variable

--- packages/mango-pagemanager/src/models/block.ts
+++ packages/mango-pagemanager/src/models/block.ts
@@ -14,11 +14,27 @@ export default class Block {
   public name: string
   public page_type: string
   public result: any[] = []
+  public render = {}
 
   constructor({ index, block }: BlockOptions) {
     this.index = index || this.index
     this.name = block && block.page_block ? block.page_block : this.name
     this.page_type = block && block.page_type ? block.page_type : this.page_type
     this.result = block && Array.isArray(block.result) ? block.result : this.result
+    this.render = this.result.reduce((a, c) => {
+      a[c.name] = c.value
+      return a
+    }, {})
+  }
+
+  /**
+   * Finds a result in the block
+   *
+   * @param {string} name
+   * @return {(object|undefined)}
+   */
+  public find(name: string) {
+    const result = this.result.find(b => b.name === name)
+    return result ? result.value : {}
   }
 }
