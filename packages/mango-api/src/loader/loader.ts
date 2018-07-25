8c454b5eb9003ac9a449fb2a18749155121374b5
Döll, Sebastian <sebastian@katallaxie.me>
2018-07-25 22:02:05 +0200

    fix ~ remove old code

--- packages/mango-api/src/loader/loader.ts
+++ packages/mango-api/src/loader/loader.ts
@@ -1,11 +1,6 @@
 import { log, error } from '../utils/log'
 
 export default class Loader {
-
-  public addResolver(name, func) {
-    this[name] = func.bind(this)
-  }
-
   public async _fetcher(ctx, url, params = {}) {
     let data
     try {
