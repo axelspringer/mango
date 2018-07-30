c0de6f8dda2fad42ec6fae1158c99ba506e24c18
Döll, Sebastian <sebastian@katallaxie.me>
2018-07-30 05:02:19 +0200

    fix ~ do not cache headers

--- packages/mango-api/src/adapter/cachable.ts
+++ packages/mango-api/src/adapter/cachable.ts
@@ -4,7 +4,6 @@ export default class Cachable {
   public status
   public statusText
   public config
-  public headers
   public request
   public timestamp
 
@@ -14,7 +13,6 @@ export default class Cachable {
     this.data = res.data
     this.status = res.status
     this.statusText = res.statusText
-    this.headers = res.headers
     this.timestamp = res.timestamp || Date.now().toString()
   }
 }
--- packages/mango-api/src/adapter/hashable.ts
+++ packages/mango-api/src/adapter/hashable.ts
@@ -4,13 +4,11 @@ export default class Hashable {
   public params
   public url
   public data
-  public headers
 
   constructor(obj) {
     this.method = obj.method
     this.params = obj.params // this assignment is faster
     this.url = obj.url
     this.data = obj.data
-    this.headers = obj.headers
   }
 }
