6401706d4a1e51dedcdf4d5c404546c5d394fb1c
Döll, Sebastian <sebastian@katallaxie.me>
2018-08-13 08:49:45 +0200

    wip

--- packages/mango-plugin-navigation/src/loader.ts
+++ packages/mango-plugin-navigation/src/loader.ts
@@ -1,26 +1,25 @@
-import { GraphQLContext } from 'graphql'
 import API from './api'
 
 export default {
 
   // fetch nav menus
-  getNavMenu: async function (ctx: GraphQLContext, id: number, args = {}) {
+  getNavMenu: async function (ctx, id: number, args = {}) {
     return this.job(ctx, [API.NavMenu, id].join('/'), args)
   },
 
   // fetch nav menu items
-  getNavItems: async function (ctx: GraphQLContext, id: number, args = {}) {
+  getNavItems: async function (ctx, id: number, args = {}) {
     return this.job(ctx, [API.NavItems, id].join('/'), args)
   },
 
   // fetch nav location
-  getNavLocation: async function (ctx: GraphQLContext, name: string, args = {}) {
+  getNavLocation: async function (ctx, name: string, args = {}) {
     const menuId = await this.job(ctx, [API.NavLocations, name].join('/'), args)
     return menuId === null ? null : this.getNavMenu(ctx, menuId, args) // this is a bit trivial
   },
 
   // fetch nav locations
-  getNavLocations: async function (ctx: GraphQLContext, args = {}) {
+  getNavLocations: async function (ctx, args = {}) {
     const res = await this.job(ctx, API.NavLocations, args) // get locations
     const locations = []
     for (const location in res) { // parse locations to a suitable
